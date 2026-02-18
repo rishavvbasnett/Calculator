123d282fc11ff22301424e3ff489f05d7f3317dc
Rishav Basnet <230183601+rishavvbasnett@users.noreply.github.com>
2026-02-17 20:15:40 -0500

    Add Logic to do arithmetic calculation. AC working
    -Support all arithmetic calculation.
    -Del, +/-, % logic missing.
    
    Suggestion:
    - Add logic for missing buttons.
    - Upgrade css for mainContainer & buttons height

--- index.js
+++ index.js
@@ -20,49 +20,56 @@ function operate(num1, operator, num2){
             return add(num1, num2)
         case "-":
             return subtract(num1,num2)            
-        case "*":
+        case "×":
             return multiply(num1, num2)           
-        case "/":
+        case "÷":
             return divide(num1, num2)
     }
 }
 
-/* Make objects from all the buttons */
-
-/* Numbers */
-const no0 = document.querySelector("#b0")
-const no1 = document.querySelector("#b1")
-const no2 = document.querySelector("#b2")
-const no3 = document.querySelector("#b3")
-const no4 = document.querySelector("#b4")
-const no5 = document.querySelector("#b5")
-const no6 = document.querySelector("#b6")
-const no7 = document.querySelector("#b7")
-const no8 = document.querySelector("#b8")
-const no9 = document.querySelector("#b9")
-
-/* operators/signs */
-const divideSign = document.querySelector("#bDivide")
-const multiplySign = document.querySelector("#Multiply")
-const subtractSign = document.querySelector("#Subtract")
-const addSign = document.querySelector("bAdd")
-const equalSign = document.querySelector("#bOperate")
-
-/* Extras */
-const deleteSign = document.querySelector("#bDelete")
-const ac = document.querySelector("#bAC")
-const percentSign = document.querySelector("#bPercent")
-const plusMinus = document.querySelector("#bSign")
-const decimal = document.querySelector("#bDecimal")
-
 
 let display = document.querySelector("#Display")
+const allButtons = document.querySelectorAll("#mainContainer button")
 
-const allNumbers = document.querySelectorAll("#mainContainer button")
-allNumbers.forEach(button=> {
-    button.addEventListener("click", e=> {
-        let buttonClicked = e.target.textContent
-        display.textContent = buttonClicked
-    })
-})
+let num1= ""
+let operatorIcon = ""
+let num2= ""
+let fullExpression = ""
+let count = 0
+let operator = ""
+answer = 0
+/* Get num1 first */
+allButtons.forEach(button => button.addEventListener("click", e => {
+    let buttonClicked = e.target.textContent
 
+    if (buttonClicked === "AC") {
+        fullExpression = ""
+    }
+    else if (buttonClicked === "=") {
+        let parts = fullExpression.split(operator)
+        num1 = Number(parts[0])
+        num2 = Number(parts[1])
+        answer = operate(num1, operator, num2)
+        fullExpression = answer
+    }
+    else if (buttonClicked === "Del") {
+        fullExpression = fullExpression.substring(0,(fullExpression.length-1))
+    } 
+    else {
+        fullExpression += buttonClicked   
+    }
+
+    if (buttonClicked === "+") {
+        operator = "+"
+    }  else if (buttonClicked === "-") {
+        operator = "-"
+    } else if (buttonClicked === "×") {
+        operator = "×"
+    } else if (buttonClicked === "÷") {
+        operator = "÷"
+    }
+
+    console.log(fullExpression)
+    display.textContent = fullExpression
+
+}))
